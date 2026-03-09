31a10d74d7e3c62781e952c38f5edbba1bf837e7
Vahnika Peddi <vahnikap@gmail.com>
2026-03-09 19:38:13 +0530

    improved expense output format

--- expense_tracker.py
+++ expense_tracker.py
@@ -18,7 +18,7 @@ while True:
     elif choice == "2":
         print("\nAll Expenses:")
         for expense in expenses:
-    print("Amount:", expense[0], "| Category:", expense[1])
+            print("Amount:", expense[0], "| Category:", expense[1])
            
 
     elif choice == "3":
